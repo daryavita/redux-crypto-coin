9059a9251186c7cc52413c2bbe55f5b8c35c8718
Darya <popova.d.v@yandex.ru>
2023-02-06 13:59:19 +0500

    fix: исправлены стили, текст, key prop

--- src/components/TrendingCoins.tsx
+++ src/components/TrendingCoins.tsx
@@ -11,28 +11,24 @@ function TrendingCoins() {
       <div className="trending__content">
         <h3 className="trending__title-coins">Coins</h3>
         <h3 className="trending__title-cap">Market cap rank</h3>
-
-        {isLoading && <p className="trending__loading">Loading...</p>}
-        {isError && <p className="trending__error">Failed to load</p>}
-
-        {data?.map((coin) => (
-          <>
-            <Link
-              className="trending__link"
-              key={coin.item.id}
-              to={`/coins/${coin.item.id}`}
-            >
-              <img
-                src={coin.item.thumb}
-                alt={coin.item.name}
-                className="trending__coin-icon"
-              ></img>
-              {coin.item.name}
-            </Link>
-            <p className="trending__market-cap">{coin.item.market_cap_rank}</p>
-          </>
-        ))}
       </div>
+
+      {isLoading && <p className="trending__loading">Loading...</p>}
+      {isError && <p className="trending__error">Failed to load</p>}
+
+      {data?.map((coin) => (
+        <div className="trending__content" key={coin.item.id}>
+          <Link className="trending__link" to={`/coins/${coin.item.id}`}>
+            <img
+              src={coin.item.thumb}
+              alt={coin.item.name}
+              className="trending__coin-icon"
+            ></img>
+            {coin.item.name}
+          </Link>
+          <p className="trending__market-cap">{coin.item.market_cap_rank}</p>
+        </div>
+      ))}
     </section>
   );
 }
--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -4,32 +4,28 @@ import { useAppSelector } from "../hooks/useSelector";
 import { CoinData } from "../models/coinDataModels";
 
 function Favourites() {
-    const { favourites } = useAppSelector((state) => state.coingecko);
-    const {resetFavourite} = useActions()
+  const { favourites } = useAppSelector((state) => state.coingecko);
+  const { resetFavourite } = useActions();
 
-    const resetFav = () => {
-        resetFavourite();
-    };
+  const resetFav = () => {
+    resetFavourite();
+  };
 
   return (
     <section className="fav">
-      <h1 className="fav__title">Favourites</h1>
-    
-        {favourites.length === 0 && (
-            <p className="fav__sub-text"> No favourites </p>
-        )}
+      <h1 className="fav__title">Wishlist</h1>
 
+      {favourites.length === 0 && (
+        <p className="fav__sub-text"> Favorites not added </p>
+      )}
 
       {favourites?.map((fav: CoinData) => (
-        <CoinCard data={fav} key={fav.id} fav={true}/>
+        <CoinCard data={fav} key={fav.id} fav={true} />
       ))}
       <p className="fav__reset-text">
-        Удалить все избранные?{" "}
-        <span
-          onClick={resetFav}
-          className="fav__reset-link"
-        >
-          Да
+        Remove everything from the wishlist?{" "}
+        <span onClick={resetFav} className="fav__reset-link">
+          Yes
         </span>
       </p>
     </section>
